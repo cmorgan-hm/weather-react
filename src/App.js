951f7c802c272f39f44f9a0430477d4488379f47
cmorgan-hm <cmorgan.hm@gmail.com>
2023-05-16 17:34:36 +0330

    Transferred the code into vscode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  let [city, setCity] = useState("");
+  let [temprature, setTemperature] = useState("");
+  let [description, setDescription] = useState("");
+  let [humidity, setHumidity] = useState("");
+  let [wind, setWind] = useState("");
+  let [icon, setIcon] = useState("");
+
+  function updateSearch(event) {
+    setCity(event.target.value);
+  }
+
+  function Start(response) {
+    setTemperature(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(response.data.weather[0].icon);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8944afa6845bd7c413a687258d3211ef&units=metric`;
+    axios.get(url).then(Start);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search for a city"
+          onChange={updateSearch}
+        />
+        <input type="submit" value="search" />
+      </form>
+      <br />
+      <div id="city"></div>
+      <div className="information">
+        <div id="temp">{`Temprature: ${Math.round(temprature)}°C`}</div>
+        <div id="description">{`Description: ${description}`}</div>
+        <div id="humidity">{`Humidity: ${humidity}%`}</div>
+        <div id="wind">{`Wind: ${Math.round(wind)} km/h`}</div>
+      </div>
+
+      <img
+        id="image"
+        src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+        alt=""
+        width="70"
+      />
     </div>
   );
 }
-
-export default App;
